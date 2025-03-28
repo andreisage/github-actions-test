47abf72c808341619b09ccb3d0a3b2c6b912eadb
Andrei Vladescu <andrei.vladescu@sage.com>
2025-03-28 10:26:01 +0000

    gradle

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
